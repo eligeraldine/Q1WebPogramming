8a010a46e9a893203db01556ea41909b4224a753
eligeraldine <162035717+eligeraldine@users.noreply.github.com>
2024-09-26 09:31:23 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,22 +1,18 @@
 document.addEventListener("DOMContentLoaded", function () {
-    // Select all links that will navigate to a new page
     const links = document.querySelectorAll('a.navigate');
 
-    // Loop through each link and add an event listener
     links.forEach(function(link) {
         link.addEventListener('click', function (event) {
-            event.preventDefault();  // Prevent the default link action
+            event.preventDefault(); 
 
-            const targetUrl = this.href;  // Get the URL of the link
+            const targetUrl = this.href;
             const pageTransition = document.querySelector('.page-transition');
 
-            // Add exit animation class
             pageTransition.classList.add('exit');
 
-            // Wait for the animation to finish before navigating
             setTimeout(function () {
                 window.location.href = targetUrl;
-            }, 500);  // The delay should match the CSS animation duration (0.5s)
+            }, 500);
         });
     });
 });
